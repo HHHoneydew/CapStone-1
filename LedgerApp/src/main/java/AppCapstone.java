dcc70fbe0167004b6285fd6ba0e2c50670adfb1c
Samantha Rivera <srivera1676595@gmail.com>
2025-04-28 22:43:48 -0700

    Homepage interface

--- LedgerApp/src/main/java/AppCapstone.java
+++ LedgerApp/src/main/java/AppCapstone.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class AppCapstone {
     public static void main(String[] args) {
+    // ryan helped me do my initial commit to the new repository
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
-
+    // creating homepage interface
         while(running) {
             System.out.println("\nHome Screen");
             System.out.println("[D] Add Deposit");
@@ -16,6 +17,7 @@ public class AppCapstone {
             String choice = scanner.nextLine().toUpperCase();
 
         }
+
 
     }
 }
